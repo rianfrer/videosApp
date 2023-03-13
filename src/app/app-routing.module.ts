7ccc4a3bd4826d1899897b851f37d4712afae6ab
Rian Felipe Ferreira da Silva <rianfrer@gmail.com>
2023-03-13 14:37:08 -0300

    Adicionei a API do The Movie DB e arrumei toda a sua interface com os serviços e Models.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,29 +4,31 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
   {
-    path: 'avatar',
-    loadChildren: () => import('./filmes/avatar/avatar.module').then( m => m.AvatarPageModule)
-  },
-  {
-    path: 'creed',
-    loadChildren: () => import('./filmes/creed/creed.module').then( m => m.CreedPageModule)
+    path: '',
+    loadChildren: () =>
+      import('./tabs/tabs.module').then((m) => m.TabsPageModule),
   },
   {
     path: 'dados-filme',
-    loadChildren: () => import('./dados-filme/dados-filme.module').then( m => m.DadosFilmePageModule)
+    loadChildren: () =>
+      import('./dados-filme/dados-filme.module').then(
+        (m) => m.DadosFilmePageModule
+      ),
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  }
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginPageModule),
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email: string = '';
+  senha: string = '';
 
-  constructor() { }
+  constructor(public toastController: ToastController, private route: Router) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  login() {
+    if (this.email === 'admin@admin.com' && this.senha === 'admin') {
+      this.route.navigateByUrl('/tabs/tab1');
+      this.presentToast('Seja bem vindo!', 'success');
+    } else {
+      this.presentToast('ERRO, usuário ou senha inválida', 'danger');
+    }
   }
 
+  async presentToast(texto: string, cor: string) {
+    const toast = await this.toastController.create({
+      message: texto,
+      color: cor,
+      duration: 2000,
+    });
+  }
 }
