a6edff8ed60a2cca520004aca69763358ffad593
Rian Felipe Ferreira da Silva <rianfrer@gmail.com>
2023-03-13 19:24:18 -0300

    Testando novamente o comando git commit.

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,5 @@
+import { IListaFilmes } from './../models/IFilmeAPI.model';
+import { FilmeService } from './../services/filme.service';
 import { DadosService } from './../services/dados.service';
 import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
@@ -7,43 +9,59 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  titulo = "Videos App";
+  titulo = 'Filmes';
 
   listaVideos: IFilme[] = [
     {
-      nome: "Avatar: O Caminho da Água (2022)",
-      lancamento: "15/12/2022",
-      duracao: "3h 12m",
+      nome: 'Avatar: O Caminho da Água (2022)',
+      lancamento: '15/12/2022',
+      duracao: '3h 12m',
       classificacao: 76,
-      cartaz: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mbYQLLluS651W89jO7MOZcLSCUw.jpg",
-      generos: ["Ficção científica", "Aventura", "Ação"],
-      pagina: "/avatar"
+      cartaz:
+        'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mbYQLLluS651W89jO7MOZcLSCUw.jpg',
+      generos: ['Ficção científica', 'Aventura', 'Ação'],
+      pagina: '/avatar',
     },
     {
-      nome: "Creed III (2023)",
-      lancamento: "02/03/2023",
-      duracao: "1h 56m",
+      nome: 'Creed III (2023)',
+      lancamento: '02/03/2023',
+      duracao: '1h 56m',
       classificacao: 73,
-      cartaz: "https://www.themoviedb.org/t/p/w440_and_h660_face/7YN6LU2sGJepnZKOa2NW2YVjq1S.jpg",
-      generos: ["Drama", "Ação"],
-      pagina: "/creed"
-    }
+      cartaz:
+        'https://www.themoviedb.org/t/p/w440_and_h660_face/7YN6LU2sGJepnZKOa2NW2YVjq1S.jpg',
+      generos: ['Drama', 'Ação'],
+      pagina: '/creed',
+    },
   ];
+
+  listaFilmes: IListaFilmes | any;
 
   constructor(
     public alertController: AlertController,
     public toastController: ToastController,
     public dadosService: DadosService,
-    public route: Router) {}
+    public filmeService: FilmeService,
+    public route: Router
+  ) {}
 
-    exibirFilme(filme: IFilme){
-      this.dadosService.guardarDados("filme", filme);
-      this.route.navigateByUrl("/dados-filme");
+  buscarFilmes(evento: any) {
+    console.log(evento.target.value);
+    const busca = evento.target.value;
+    if (busca && busca.trim() !== '') {
+      this.filmeService.buscarFilmes(busca).subscribe((dados) => {
+        console.log(dados);
+        this.listaFilmes = dados;
+      });
     }
+  }
+
+  exibirFilme(filme: IFilme) {
+    this.dadosService.guardarDados('filme', filme);
+    this.route.navigateByUrl('/dados-filme');
+  }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -70,14 +88,12 @@ export class Tab1Page {
   }
 
   async apresentarToast() {
-      const toast = await this.toastController.create({
-        message: 'Filme adicionado aos favoritos!',
-        duration: 2000,
-        color: "success"
-      });
+    const toast = await this.toastController.create({
+      message: 'Filme adicionado aos favoritos!',
+      duration: 2000,
+      color: 'success',
+    });
 
     await toast.present();
-
   }
 }
-
